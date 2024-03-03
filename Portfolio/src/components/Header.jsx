898ad781a0203ba424df7456f4a5c55f6e165c60
Bdoherty1 <bdoherty988@gmail.com>
2024-03-03 14:00:28 -0600

    landing page header and hero.hero img all setup and looks good with responsive layout, tailwind was used for stlying

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -1,35 +1,49 @@
 import React from 'react'
 
 function Header() {
-    const menuList= [
+    const menuList = [
         {
-            id:1,
+            id: 1,
             title: 'HOME'
         },
         {
-            id:1,
+            id: 1,
             title: 'ABOUT'
         },
         {
-            id:1,
+            id: 1,
             title: 'PORTOFLIO'
         },
         {
-            id:1,
+            id: 1,
             title: 'CONTACT'
         },
         {
-            id:1,
+            id: 1,
             title: 'RESMUE'
         },
     ]
-  return (
-    <div>
-        <div>
-            <h2 clasName='text-[24px] font-bold text-white'>Header <span> Brian </span></h2>
+    return (
+        <div className='flex items-center jsutify-between'>
+            <div>
+                <h2 clasName='text-[24px] font-bold text-white'>Brian
+                    <span className='text-red-500'> Doherty </span></h2>
+            </div>
+            <div className='hidden md:flex gap-4'>
+                {menuList.map((item) => (
+                    <div>
+                        <h2 className='text-white
+                        hover:border-[1px] border-red-500 rounded-full
+                        text-[15px] px-3 py-1 cursor-pointer'>{item.title}</h2>
+                    </div>
+                ))}
+                <h2 className='text-white
+                hover:border-[1px] border-red-500 rounded-full
+                text-[15px] px-3 py-1 cursor-pointer hover:bg-gradient-to-r from-red-500 to-red-800'>HIRE ME</h2>
+            </div>
+            
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='grid grid-cols-1 md:grid-cols-2 pt-10 items-center'>
+    <div className='mb-10'>
+        <p className='text-white text-[16px]'>HELLO, WELCOME TO MY SITE</p>
+        <p className='text-white text-[40px] font-bold'>My Name Is Brian Doherty</p>
+        <p className='text-white text-[40px] font-bold'>I'm Full Stack Developer</p>
+        <p className='text-white text-[40px] font-bold'>(MREN)</p>
+        <div className='mt-4 flex gap-4'>
+        <button className='text-white text-[12px] px-3 pb-2 py-2
+             bg-red-500 rounded-full border-[2px] hover:border-white'>SEE PORTFOLIO</button>
+                <button className='text-white text-[12px] px-3 pb-2 py-2
+             bg-red-500 rounded-full border-[2px] hover:border-white'>CONTACT ME</button>
+            </div>
+            </div>
+        <div className='flex justify-center '>
+        <div className='h-[320px] w-[250px] p-2 rounded-[20px] bg-gradient-to-b from-[#FF004D]  to-[#2A26B8]'>
+            <img src='../imgs/heroimg.jpg'
+            className='h-full w-[250px] object-cover rounded-[20px]'
+            />
+        </div>
+        </div>
+    </div>
   )
 }
+
 
 export default Hero
